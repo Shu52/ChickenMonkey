6a0cc23edfb3c5ea33a9c915cb9b01bdfca4b37c
Shuaib Sajid <shusajid@gmail.com>
2018-05-24 08:55:29 -0500

    added to js

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 for (let i=1;i<=100;i++){
  let int = "";
- if (i % 5 == 0)
+ if (i % 5 === 0)
  {
      int += "Chicken";
  }
- if (i % 7 == 0){
+ if (i % 7 === 0){
      int += "Monkey";
  }  
- if (int == ""){
+ if (int === ""){
      int = i;
  }
  console.log(int)
